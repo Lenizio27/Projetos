49b84950cfc2308e06ab8080c3aab61bc1a12637
Lenizio27 <lenizio27@gmail.com>
2025-07-09 23:41:38 -0300

    Inicio do Projeto
    importando bibliotecas, criando pastas e rotas

---
+++ To-do-list_React/To-do-list_React/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Paths from './Paths/Paths'
+
+function App() {
+
+  return (
+    <>
+      <Paths/>
+    </>
+  )
+}
+
+export default App
---
+++ To-do-list_React/To-do-list_React/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <>
+            <h1>Home_Page</h1>
+            <p>To-Do List</p>
+        </>
+     );
+}
+ 
+export default Home;
